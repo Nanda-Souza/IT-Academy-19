6ba9ab14849ebf78581607982afa6543a7741d81
Oliveira <fernanda.oliveira3@dellteam.com>
2023-09-16 09:36:13 -0300

    Lab 7

---
+++ C#/Lab7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{   
+static void Main(string[] args)
+{
+ IEstadoBinario[] lista = new IEstadoBinario[2];
+ lista[0] = new Lampada(110, 60);
+ lista[0].Ligar();
+ lista[1] = new TermometroEletrico();
+
+ for (int i = 0; i < 2; i++)
+ {
+ Console.WriteLine(lista[i].Estado);
+ }
+}
+}
